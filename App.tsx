3b4661fa2b409b047a10034cf532db7a63dba76d
Not4PplPrsn <st10470237@vcconnect.edu.za>
2025-09-19 14:38:53 +0200

    Replace old code with the code from the guide Mr j gave us so i and changed the colour for all the things. i also added the use state import

--- App.tsx
+++ App.tsx
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, } from 'react-native';
+import { useState } from "react";
 
+const [users, useState] = [
+  { id: "1", name: "Siya", age: 25 }, useState('')
+  { id: "2", name: "Caryn", age: 30 },
+  { id: "3", name: "Jaco", age: 22 },
+  { id: "4", name: "Mihle", age: 28 },
+];
+const UserItem = ({ name, age }: { name: string; age: number }) => (
+  <View style={styles.item}>
+    <Text style={styles.name}>{name}</Text>
+    <Text style={styles.age}>Age: {age}</Text>
+  </View>
+);
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>👋 My First FlatList</Text>
+      <FlatList
+	      data={users}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <UserItem name={item.name} age={item.age} />}
+      />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#36e9c2ff",
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 15,
+  },
+  item: {
+    backgroundColor: "#2ca0d6ff",
+    padding: 15,
+    marginVertical: 8,
+    borderRadius: 8,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  age: {
+    fontSize: 14,
+    color: "#555",
   },
 });
+
+
